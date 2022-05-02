21f5bc1630c06acca1293869b59cb82771c33465
Josirocha <josi1992@gmail.com>
2022-05-01 22:24:24 -0300

    Js do base 64

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ select.addEventListener('change', function () {
         steps.style.display = 'none'
     }
 })
+
+function criptografia() {
+    const text = document.getElementById('textToDecode')
+    const method = document.getElementById('selection')
+    const steps = document.getElementById('passos')
+    if (method.value === "cesar") {
+        toCesar(text, steps)
+    } else {
+        toB64(text)
+    }
+}
+
+function toCesar(text, steps) {
+
+}
+
+function toB64(text) {
+    const code = document.querySelector('input[name="code"]:checked').value
+    if (code === "codificar") {
+        document.getElementById("decodedText").value = btoa(text.value)
+    } else if (code === "decodificar") {
+        document.getElementById("decodedText").value = atob(text.value)
+    } else {
+        alert("selecione codificar ou decodificar")
+    }
+}
